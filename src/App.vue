9dc49991234150ca22998063a9199d04e7521cdd
Feliphe Paz <feliphe.paz@enjoei.com.br>
2024-12-13 16:48:29 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Navbar from '@/components/navbar.vue'
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
